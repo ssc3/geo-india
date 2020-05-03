3fa34f1a7eebca9f9c3ee4e981efa9550aa01f48
Shubho <shubhojit.chattopadhyay@datadoghq.com>
2020-05-03 17:11:29 -0400

    Basic plot display from intellij

--- src/run.py
+++ src/run.py
@@ -1,1 +1,20 @@
-print("HELLO")
+# required imports
+import xlrd
+import random
+import pandas as pd
+import seaborn as sns
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import shapefile as shp
+
+# reading the state wise shapefile of India in a GeoDataFrame and preview it
+
+fp = "../gadm36_IND_shp/gadm36_IND_2.shp"
+map_df = gpd.read_file(fp)
+map_df.head()
+
+
+# Plot the default map
+
+map_df.plot()
+plt.show()
